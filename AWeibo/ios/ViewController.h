44b6136dffbebe64a678720b6eae4ccc07afff8e
Lokiwang <wangjian1009@gmail.com>
2012-05-14 18:54:24 +0800

    first add xcode project

---
+++ AWeibo/ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AWeibo
+//
+//  Created by Loki on 12-5-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
