afbf00563361380361a4e31a0f07207cb65100c2
Lokiwang <wangjian1009@gmail.com>
2012-05-20 10:17:56 +0800

    update AWeibo project

--- AWeibo/ios/config.mk
+++ AWeibo/ios/config.mk
@@ -2,9 +2,10 @@ product:=AWeibo
 $(product).type:=progn install
 $(product).depends:=libmmseg \
                     cpepp_cfg \
-                    gd_app gd_evt gd_net gdpp_nm cpepp_dp gdpp_app gdpp_timer
+                    gd_app gd_evt gd_net gdpp_nm cpepp_dp gdpp_app gdpp_timer \
+                    weibo_data
 $(product).libraries:=
-$(product).c.sources:= $(wildcard $(product-base)/ios/*.m)
+$(product).c.sources:= $(wildcard $(product-base)/*.m)
 $(product).c.frameworks:=CoreGraphics UIKit Foundation
 $(product).c.flags.cpp:=-I$(CPDE_ROOT)
 
@@ -17,7 +18,7 @@ $(product).c.export-symbols:= \
                         app_net_env_global_init app_net_env_global_fini
 
 $(product).output:=AWeibo.app
-$(product).install:= $(call def-cvt-file,AWeibo/ios/AWeibo-Info.plist, AWeibo.app/Info.plist, AWeibo/product.def) \
+$(product).install:= $(call def-cvt-file,AWeibo/ios/AWeibo-Info.plist, AWeibo.app/Info.plist, AWeibo/ios/product.def) \
                      $(call def-cvt-file-ex,AWeibo/ios/en.lproj/ViewController.xib, AWeibo.app/ViewController.nib, xib-nib) \
                      $(call def-copy-dir-r,AWeibo/Resources/etc, AWeibo.app/etc) \
                      $(call def-copy-dir,AWeibo/Resources, AWeibo.app, png PNG caf txt mp3)
